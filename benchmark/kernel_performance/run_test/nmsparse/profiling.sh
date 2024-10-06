81cbab8b5380f8d5d8bf6fa029c653c128114aca
M-H482 <2268845318@qq.com>
2024-10-06 17:26:41 +0800

    add nmsparse profiling shell

---
+++ benchmark/kernel_performance/run_test/nmsparse/profiling.sh
@@ -0,0 +1,4 @@
+ncu --set full -c 3 -f -o nmsparse_4kx4kx4k_50 ./nmsparse 4096 4096 4096 32 0.5
+ncu --set full -c 3 -f -o nmsparse_4kx4kx4k_625 ./nmsparse 4096 4096 4096 32 0.625
+ncu --set full -c 3 -f -o nmsparse_4kx4kx4k_75 ./nmsparse 4096 4096 4096 32 0.75
+ncu --set full -c 3 -f -o nmsparse_4kx4kx4k_875 ./nmsparse 4096 4096 4096 32 0.875
