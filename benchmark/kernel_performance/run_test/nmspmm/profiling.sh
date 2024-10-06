bffe573ba0a78e85ccd91aeece7b56409098e4c2
M-H482 <2268845318@qq.com>
2024-10-06 17:25:28 +0800

    add  nmspmm profiling results, shell

---
+++ benchmark/kernel_performance/run_test/nmspmm/profiling.sh
@@ -0,0 +1,4 @@
+ncu --set full -c 3 -f -o nmspmm_4kx4kx4k_50 ./profile 4096 4096 4096 32 0.5
+ncu --set full -c 3 -f -o nmspmm_4kx4kx4k_625 ./profile 4096 4096 4096 32 0.625
+ncu --set full -c 3 -f -o nmspmm_4kx4kx4k_75 ./profile 4096 4096 4096 32 0.75
+ncu --set full -c 3 -f -o nmspmm_4kx4kx4k_875 ./profile 4096 4096 4096 32 0.875
